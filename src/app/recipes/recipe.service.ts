9ec3b0d94a08b47cd1b38bdac7819883e5c90822
Ryan Jacobo <ryanjacobo@yahoo.com>
2023-01-03 11:51:09 -0500

    commented dummy data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,26 +8,24 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  // private recipes: Recipe[] = [
-  //   new Recipe(
-  //     'Sinigang',
-  //     'Pork belly with tamarind soup.',
-  //     'https://www.kawalingpinoy.com/wp-content/uploads/2013/01/sinigang-baboy-7-768x1024.jpg',
-  //     [new Ingredient('Pork belly cubes', 1), new Ingredient('Veggies', 3)]
-  //   ),
-  //   new Recipe(
-  //     'Beef Caldereta',
-  //     'Beef shoulder stew.',
-  //     'https://www.kawalingpinoy.com/wp-content/uploads/2019/04/spicy-beef-caldereta-2.jpg',
-  //     [
-  //       new Ingredient('Beef shoulder cubes', 1),
-  //       new Ingredient('Tomato paste', 1),
-  //       new Ingredient('Carrots', 1),
-  //     ]
-  //   ),
-  // ];
-
-  private recipes: Recipe[] = [];
+  private recipes: Recipe[] = [
+    // new Recipe(
+    //   'Sinigang',
+    //   'Pork belly with tamarind soup.',
+    //   'https://www.kawalingpinoy.com/wp-content/uploads/2013/01/sinigang-baboy-7-768x1024.jpg',
+    //   [new Ingredient('Pork belly cubes', 1), new Ingredient('Veggies', 3)]
+    // ),
+    // new Recipe(
+    //   'Beef Caldereta',
+    //   'Beef shoulder stew.',
+    //   'https://www.kawalingpinoy.com/wp-content/uploads/2019/04/spicy-beef-caldereta-2.jpg',
+    //   [
+    //     new Ingredient('Beef shoulder cubes', 1),
+    //     new Ingredient('Tomato paste', 1),
+    //     new Ingredient('Carrots', 1),
+    //   ]
+    // ),
+  ];
 
   constructor(private shoppinglistService: ShoppingListService) {}
 
